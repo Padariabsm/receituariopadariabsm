626668a25ba5d010955966284de9a10f7350f0bf
Padariabsm <139655939+Padariabsm@users.noreply.github.com>
2023-07-16 20:16:19 -0300

    Update fireBase.js

--- fireBase.js
+++ fireBase.js
@@ -1,16 +1,16 @@
- <script>
-    // Configuração do Firebase
-    var firebaseConfig = {
-     apiKey: "AIzaSyDdKuFRrZdZfmYyFHefYpsqeNleEPMSSFA",
+
+// fireBase.js
+
+// Configuração do Firebase
+var firebaseConfig = {
+   apiKey: "AIzaSyDdKuFRrZdZfmYyFHefYpsqeNleEPMSSFA",
       authDomain: "padariareceituario.firebaseapp.com",
       projectId: "padariareceituario",
       storageBucket: "padariareceituario.appspot.com",
       messagingSenderId: "159527859464",
       appId: "1:159527859464:web:8888b169348e12fe1868bd"
-    };
+};
 
-    // Inicializar o Firebase com a configuração
-    firebase.initializeApp(firebaseConfig);
+// Inicializar o Firebase com a configuração
+firebase.initializeApp(firebaseConfig);
 
-    
-  </script>
