96b2921a46bf74d8d2dadebcab5291586a5c03b0
Padariabsm <139655939+Padariabsm@users.noreply.github.com>
2023-07-16 19:04:48 -0300

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+// Configuração do Firebase
+var firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "padariareceituario.firebaseapp.com",
+  projectId: "padariareceituario",
+  storageBucket: "padariareceituario.appspot.com",
+  messagingSenderId: "159527859464",
+  appId: "1:159527859464:web:8888b169348e12fe1868bd"
+};
+
+// Exportar a configuração do Firebase
+export { firebaseConfig };
