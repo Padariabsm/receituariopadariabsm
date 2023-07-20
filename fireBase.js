0f3808157978f339d1f43f26b912ba95ca3e97c4
Padariabsm <139655939+Padariabsm@users.noreply.github.com>
2023-07-19 23:52:16 -0300

    Add files via upload

---
+++ fireBase.js
@@ -0,0 +1,15 @@
+
+// fireBase.js
+
+// Configuração do Firebase
+var firebaseConfig = {
+   apiKey: "AIzaSyDdKuFRrZdZfmYyFHefYpsqeNleEPMSSFA",
+      authDomain: "padariareceituario.firebaseapp.com",
+      projectId: "padariareceituario",
+      storageBucket: "padariareceituario.appspot.com",
+      messagingSenderId: "159527859464",
+      appId: "1:159527859464:web:8888b169348e12fe1868bd"
+};
+
+// Inicializar o Firebase com a configuração
+firebase.initializeApp(firebaseConfig);
